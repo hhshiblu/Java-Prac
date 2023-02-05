11e38970bb3d860eef91e352000f83f51c80273a
hhshiblu <hhshiblu5555@gmail.com>
2023-02-05 20:44:25 +0600

    first commit

---
+++ class 1/New folder/Main.java
@@ -0,0 +1,15 @@
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+        Student obj1 = new Student();
+        obj1.age=20;
+        obj1.cgpa=5.6f;
+        obj1.name="hasan";
+        obj1.display();
+
+    }
+}
---
+++ class 1/New folder/Student.java
@@ -0,0 +1,13 @@
+public class Student {
+    int age;
+    float cgpa;
+  String name;
+   void display (){
+
+        System.out.println(age);
+        System.out.println(cgpa);
+        System.out.println(name);
+    }
+
+    
+}
